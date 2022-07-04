c2bdfc426a331de80a85af122d6061ebc45ad770
Andrés Gutiérrez <andres_mau.gutierrez@uao.edu.co>
2022-07-04 14:46:36 -0500

    Paginación y relación muchos a muchos

--- routes/products.router.js
+++ routes/products.router.js
@@ -8,24 +8,26 @@ const ProductsService = require('./../services/product.service');
 const validatorHandler = require('./../middlewares/validator.handler');
 
 // Importamos los esuqemas del producto
-const { createProductSchema, updateProductSchema, getProductSchema } = require('./../schemas/product.schema');
+const { createProductSchema, updateProductSchema, getProductSchema, queryProductSchema } = require('./../schemas/product.schema');
 
 // Importamos router
 const router = express.Router();
 
-// Llamamos a la clase servicio y creamos un nuevo servicio [Al crear un nuevo servicio se crean 100 datos fake]
+// Llamamos a la clase servicio y creamos un nuevo servicio
 const service = new ProductsService();
 
 // Función para obtener todos los productos
-router.get('/', async (req, res) => {
-  const products = await service.find();
-  res.json(products);
-});
-
-// Función para responder a la petición en un /filter
-router.get('/filter', (req, res) => {
-  res.send('Yo soy un filter');
-});
+router.get('/',
+  validatorHandler(queryProductSchema, 'query'),
+  async (req, res, next) => {
+    try {
+      const products = await service.find(req.query);
+      res.json(products);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 // Función para obtener un producto
 router.get('/:id',
@@ -44,12 +46,17 @@ router.get('/:id',
 // Función para crear un producto
 router.post('/',
   validatorHandler(createProductSchema, 'body'),
-  async (req, res) => {
-    const body = req.body;
-    const newProduct = await service.create(body);
-    res.status(201).json(newProduct);
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newProduct = await service.create(body);
+      res.status(201).json(newProduct);
+    } catch (error) {
+      next(error);
+    }
   }
 );
+
 
 // Función para actualizar un producto
 router.patch('/:id',
@@ -68,10 +75,17 @@ router.patch('/:id',
 );
 
 // Función para eliminar un producto
-router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-  const rta = await service.delete(id);
-  res.json(rta);
-});
+router.delete('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({id});
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
